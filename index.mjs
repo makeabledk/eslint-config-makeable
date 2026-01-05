073c902cb45302212affd1741acdbd220a45e3b8
Lasse Borgen <lasse@makeable.dk>
2026-01-05 15:41:41 +0100

    Enabled promise-executor-returns

--- index.mjs
+++ index.mjs
@@ -78,6 +78,7 @@ export default [
             'no-undef': 0,
             'no-void': 0,
             'no-continue': 0,
+            'no-promise-executor-return': 0,
             'spaced-comment': 1,
             'no-case-declarations': 1,
             'import/no-extraneous-dependencies': [
